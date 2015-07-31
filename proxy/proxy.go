98df3648c5645bd71585667e4e9049d11171255c
Anielkis Herrera <anielkis@gmail.com>
2015-07-31 10:42:23 -0500

    Update

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,20 +1,19 @@
 package proxy
 
 import (
-	"encoding/binary"
+	"github.com/DimShadoWWW/power-pg/common"
 	"fmt"
-	"io"
 	"net"
 	"os"
-
-	"github.com/DimShadoWWW/power-pg/common"
+	"io"
+	"encoding/binary"
 )
+
 
 var (
 	connid = uint64(0)
 )
 
-// Start function
 func Start(localHost, remoteHost *string, powerCallback common.Callback) {
 	fmt.Printf("Proxying from %v to %v\n", localHost, remoteHost)
 
@@ -30,16 +29,17 @@ func Start(localHost, remoteHost *string, powerCallback common.Callback) {
 		connid++
 
 		p := &proxy{
-			lconn:  *conn,
-			laddr:  localAddr,
-			raddr:  remoteAddr,
-			erred:  false,
-			errsig: make(chan bool),
-			prefix: fmt.Sprintf("Connection #%03d ", connid),
+			lconn:    conn,
+			laddr:    localAddr,
+			raddr:    remoteAddr,
+			erred:    false,
+			errsig:   make(chan bool),
+			prefix:   fmt.Sprintf("Connection #%03d ", connid),
 		}
 		go p.start(powerCallback)
 	}
 }
+
 
 func getResolvedAddresses(localHost, remoteHost *string) (*net.TCPAddr, *net.TCPAddr) {
 	laddr, err := net.ResolveTCPAddr("tcp", *localHost)
@@ -49,21 +49,24 @@ func getResolvedAddresses(localHost, remoteHost *string) (*net.TCPAddr, *net.TCP
 	return laddr, raddr
 }
 
-func getListener(addr *net.TCPAddr) *net.TCPListener {
+
+func getListener(addr *net.TCPAddr) (*net.TCPListener) {
 	listener, err := net.ListenTCP("tcp", addr)
 	check(err)
 	return listener
 }
 
+
 type proxy struct {
 	sentBytes     uint64
 	receivedBytes uint64
 	laddr, raddr  *net.TCPAddr
-	lconn, rconn  net.TCPConn
+	lconn, rconn  *net.TCPConn
 	erred         bool
 	errsig        chan bool
 	prefix        string
 }
+
 
 func (p *proxy) err(s string, err error) {
 	if p.erred {
@@ -76,17 +79,17 @@ func (p *proxy) err(s string, err error) {
 	p.erred = true
 }
 
+
 func (p *proxy) start(powerCallback common.Callback) {
-	// defer p.lconn.conn.Close()
+	defer p.lconn.Close()
 	//connect to remote
 	rconn, err := net.DialTCP("tcp", nil, p.raddr)
 	if err != nil {
 		p.err("Remote connection failed: %s", err)
 		return
 	}
-	p.rconn = *rconn
-	// p.rconn.alive = true
-	// defer p.rconn.conn.Close()
+	p.rconn = rconn
+	defer p.rconn.Close()
 	//bidirectional copy
 	go p.pipe(p.lconn, p.rconn, powerCallback)
 	go p.pipe(p.rconn, p.lconn, nil)
@@ -94,144 +97,34 @@ func (p *proxy) start(powerCallback common.Callback) {
 	<-p.errsig
 }
 
-func (p *proxy) pipe(src, dst net.TCPConn, powerCallback common.Callback) {
+
+func (p *proxy) pipe(src, dst *net.TCPConn, powerCallback common.Callback) {
 	//data direction
 	islocal := src == p.lconn
 	//directional copy (64k buffer)
-	buff := make(readBuf, 0xffff)
-	// newPacket := true
-	// var msg string
-	// remainingBytes := 0
-	if islocal {
-		for {
-			// var r readBuf
-			n, err := src.Read(buff)
-			if err != nil {
-				p.err("Read failed '%s'\n", err)
-				return
-			}
-			fmt.Printf("Readed bytes: %d\n", n)
-			b := buff[:n]
-			//write out result
-			n, err = dst.Write(b)
-			if err != nil {
-				p.err("Write failed '%s'\n", err)
-				return
-			}
-
-			// 	r = buff[:n]
-			// 	fmt.Printf("%#v\n", buff[:n])
-			// 	if remainingBytes > 0 {
-			// 		if remainingBytes <= n {
-			// 			newPacket = true
-			// 			msg = msg + string(r.next(remainingBytes))
-			// 			remainingBytes = n - remainingBytes
-			// 			fmt.Println(msg)
-			// 		} else {
-			// 			newPacket = false
-			// 			msg = msg + string(r.next(remainingBytes))
-			// 			remainingBytes = remainingBytes - n
-			// 		}
-			// 	}
-			// 	fmt.Printf("Remaining bytes: %d\n", remainingBytes)
-			// NewP:
-			// 	if newPacket {
-			// 		remainingBytes = 0
-			// 		newPacket = false
-			// 		msg = ""
-			// 		t := r.byte()
-			// 		n = n - 1
-			// 		fmt.Println(t)
-			// 		switch t {
-			// 		case query:
-			// 			// c.rxReadyForQuery(r)
-			// 			remainingBytes = r.int32()
-			// 			remainingBytes = remainingBytes - 4
-			// 			if remainingBytes > 0 {
-			// 				if remainingBytes <= n {
-			// 					newPacket = true
-			// 					msg = msg + string(r.next(remainingBytes))
-			// 					remainingBytes = n - remainingBytes
-			// 					fmt.Println(msg)
-			// 					goto NewP
-			// 				} else {
-			// 					newPacket = false
-			// 					msg = msg + string(r.next(remainingBytes))
-			// 					remainingBytes = remainingBytes - n
-			// 				}
-			// 				fmt.Printf("Remaining bytes: %d\n", remainingBytes)
-			// 			}
-			// 		// case rowDescription:
-			// 		// case dataRow:
-			// 		// case bindComplete:
-			// 		// case commandComplete:
-			// 		// 	commandTag = CommandTag(r.readCString())
-			// 		default:
-			// 			// if e := c.processContextFreeMsg(t, r); e != nil && softErr == nil {
-			// 			// 	softErr = e
-			// 			// }
-			// 		}
-			// 	}
-			// 	r = append(r, buff[:]...)
-
-			// fmt.Println("a")
-			// c := src
-			// c.reader = bufio.NewReader(src.conn)
-			// c.mr.reader = c.reader
-			//
-			// var t byte
-			// var r *msgReader
-			// fmt.Println("b")
-			// t, r, err := c.rxMsg()
-			// fmt.Println("c")
-			// if err != nil {
-			// 	fmt.Println(err)
-			// 	return
-			// }
-			// fmt.Println("d")
-			//
-			// fmt.Printf("t: %#v\n", t)
-
-			// n, err := src.Read(buff)
-			// if err != nil {
-			// 	p.err("Read failed '%s'\n", err)
-			// 	return
-			// }
-			// b := buff[:n]
-			// //show output
-			//
-			//
-			// b = getModifiedBuffer(b, powerCallback)
-			// n, err = dst.Write(b)
-			// //
-			// //write out result
-			// n, err = dst.Write(b)
-			// if err != nil {
-			// 	p.err("Write failed '%s'\n", err)
-			// 	return
-			// }
+	buff := make([]byte, 0xffff)
+	for {
+		n, err := src.Read(buff)
+		if err != nil {
+			p.err("Read failed '%s'\n", err)
+			return
 		}
-	} else {
-		for {
-			n, err := src.Read(buff)
-			if err != nil {
-				p.err("Read failed '%s'\n", err)
-				return
-			}
-			b := buff[:n]
+		b := buff[:n]
+		//show output
+		if islocal {
+			b = getModifiedBuffer(b, powerCallback)
+			n, err = dst.Write(b)
+		} else {
 			//write out result
 			n, err = dst.Write(b)
-			if err != nil {
-				p.err("Write failed '%s'\n", err)
-				return
-			}
-			if err != nil {
-				p.err("Write failed '%s'\n", err)
-				return
-			}
+		}
+		if err != nil {
+			p.err("Write failed '%s'\n", err)
+			return
 		}
 	}
 }
+
 
 func getModifiedBuffer(buffer []byte, powerCallback common.Callback) []byte {
 	if powerCallback == nil || len(buffer) < 1 || string(buffer[0]) != "Q" || string(buffer[5:11]) != "power:" {
@@ -240,6 +133,7 @@ func getModifiedBuffer(buffer []byte, powerCallback common.Callback) []byte {
 	query := powerCallback(string(buffer[5:]))
 	return makeMessage(query)
 }
+
 
 func makeMessage(query string) []byte {
 	queryArray := make([]byte, 0, 6+len(query))
@@ -251,6 +145,7 @@ func makeMessage(query string) []byte {
 
 }
 
+
 func check(err error) {
 	if err != nil {
 		warn(err.Error())
@@ -258,6 +153,8 @@ func check(err error) {
 	}
 }
 
+
 func warn(f string, args ...interface{}) {
 	fmt.Printf(f+"\n", args...)
 }
+
