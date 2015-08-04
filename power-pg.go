41e47e0aa2952deefc7453c266a0e002fdb52c5a
Anielkis Herrera <anielkis@gmail.com>
2015-08-04 12:19:43 -0500

    Update

--- power-pg.go
+++ power-pg.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"log"
@@ -141,9 +142,9 @@ func main() {
 						var varsIdx []int
 						for i := 0; i < totalVar; i++ {
 							fmt.Printf("2 newMsg   ----->%#v\n", newMsg)
-							varLen := newMsg.Int32()
-							fmt.Println("aa   -----> ", string(varLen))
-							// varLen := int(binary.BigEndian.Uint32(aa))
+							aa := newMsg.Next(4)
+							fmt.Printf("aa   -----> %#v\n", aa)
+							varLen := int(binary.LittleEndian.Uint32(aa))
 							fmt.Printf("varLen ----->%v\n", varLen)
 							fmt.Printf("newMsg   ----->%#v\n", newMsg)
 							vars[i] = string(newMsg.Next(varLen))
