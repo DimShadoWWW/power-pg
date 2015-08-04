ba8e5059214b09f57f948279c20304d7864163db
Anielkis Herrera <anielkis@gmail.com>
2015-08-04 07:11:34 -0500

    Update

--- power-pg.go
+++ power-pg.go
@@ -71,8 +71,14 @@ func main() {
 						sepIdx := strings.Index(string(msg.Content), string([]byte{0, 1, 0, 0}))
 						if sepIdx == -1 || sepIdx+4 > len(msg.Content) {
 							sepIdx = len(msg.Content) - 4
+							if sepIdx == -1 {
+								sepIdx = len(msg.Content)
+							}
 						} else {
 							sepIdx = len(msg.Content) - 1
+							if sepIdx == -1 {
+								sepIdx = len(msg.Content)
+							}
 						}
 					}
 					fmt.Printf("SEP index ----->%v\n", sepIdx)
