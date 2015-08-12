723355a47d33753808765d2cf678213935e39d8c
Anielkis Herrera <anielkis@gmail.com>
2015-08-12 10:22:42 -0500

    Update

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -174,6 +174,7 @@ func (p *proxy) pipe(src, dst net.TCPConn, msgBytes chan []byte, msgCh chan Pkg,
 					// c.rxReadyForQuery(r)
 					log.Debug("PostgreSQL pkg type: %s\n", string(t))
 					remainingBytes = r.Int32()
+					n = n - 4
 					if msgCh != nil {
 						log.Debug("1 Remaining bytes: %d \tn: %d\n", remainingBytes, n)
 					}
